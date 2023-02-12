1046752fdc06ca2cce5f8e323d40d439446433d7
viktor0980 <viktor0980@gmail.com>
2023-02-12 18:38:05 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+   const collection = document.querySelectorAll(".hot");
+    let hot = document.createTextNode("🔥");
+    collection[0].appendChild(hot);
 });
